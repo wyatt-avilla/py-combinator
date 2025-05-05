33621f33ecc6a0ea3386b26c516e692864a228f7
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-05-04 22:23:23 -0700

    int iterator new, map, uncalled getter

--- python/tests/test_all.py
+++ python/tests/test_all.py
@@ -1,6 +1,26 @@
-import pytest
-import py_combinator
+import py_combinator._py_combinator as rs
+from py_combinator import IteratorOfInt
 
 
-def test_sum_as_string():
-    assert py_combinator.sum_as_string(1, 1) == "2"
+def test_map_addition() -> None:
+    it = IteratorOfInt([1, 2, 3])
+    mapped = it.map(lambda x: x + 1).go()
+    assert list(mapped) == [2, 3, 4]
+
+
+def test_chaining() -> None:
+    c1 = rs.IteratorOfInt([1, 2, 3])
+    c2 = c1.map(lambda x: x)
+    c3 = c2.map(lambda x: x)
+
+    assert c1 is c2 is c3
+
+
+def test_uncalled_count() -> None:
+    c1 = rs.IteratorOfInt([1, 2, 3])
+
+    assert c1.uncalled == 0
+
+    c1.map(lambda x: x)
+
+    assert c1.uncalled == 1
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,49 @@
-use pyo3::prelude::*;
+use std::collections::VecDeque;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+use pyo3::{
+    prelude::*,
+    types::{PyFunction, PyInt, PyList},
+};
+
+enum SupportedIterableTypes {
+    PyInt(pyo3::types::PyInt),
 }
 
-/// A Python module implemented in Rust.
+// TODO: generate with macro based on `SupportedIterableTypes`
+#[pyclass]
+struct IteratorOfInt {
+    it: Box<dyn Iterator<Item = Py<PyInt>> + Send + Sync>,
+    to_apply: VecDeque<Py<PyFunction>>,
+}
+
+#[pymethods]
+impl IteratorOfInt {
+    #[new]
+    fn py_new(list: &Bound<'_, PyList>) -> PyResult<Self> {
+        let ints = list
+            .iter()
+            .map(|x| x.downcast_into::<PyInt>().map(pyo3::Bound::unbind))
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(IteratorOfInt {
+            it: Box::new(ints.into_iter()),
+            to_apply: VecDeque::new(),
+        })
+    }
+
+    fn map<'a>(mut slf: PyRefMut<'a, Self>, f: Bound<'_, PyFunction>) -> PyRefMut<'a, Self> {
+        slf.to_apply.push_back(f.unbind());
+        slf
+    }
+
+    #[getter]
+    fn uncalled(&self) -> usize {
+        self.to_apply.len()
+    }
+}
+
 #[pymodule]
 fn _py_combinator(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    let _ = m.add_class::<IteratorOfInt>();
     Ok(())
 }
