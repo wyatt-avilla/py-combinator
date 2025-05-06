cc1f6588c56054c5d05f6476e90f31dd3a0f6a86
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-05-06 15:49:33 -0700

    build rust iterator from python list and return as python list

--- python/tests/test_all.py
+++ python/tests/test_all.py
@@ -1,15 +1,38 @@
 import py_combinator._py_combinator as rs
-from py_combinator import IteratorOfInt
+from py_combinator import AnyIterator
 
 
 def test_map_addition() -> None:
-    it = IteratorOfInt([1, 2, 3])
-    mapped = it.map(lambda x: x + 1).go()
-    assert list(mapped) == [2, 3, 4]
+    nums = [1, 2, 3]
+    it = AnyIterator(nums)
+    f = lambda x: x + 1
+
+    lib_mapped = it.map(f).to_list()
+    native_mapped = list(map(f, nums))
+
+    assert lib_mapped == native_mapped
+
+
+def test_list_conversion() -> None:
+    nums = [1, 2, 3]
+
+    it = AnyIterator(nums)
+
+    assert it.to_list() == nums
+
+
+def test_iterator_consumption() -> None:
+    nums = [1, 2, 3]
+
+    it = AnyIterator(nums)
+
+    it.to_list()
+
+    assert len(it.to_list()) == 0
 
 
 def test_chaining() -> None:
-    c1 = rs.IteratorOfInt([1, 2, 3])
+    c1 = rs.AnyIterator([1, 2, 3])
     c2 = c1.map(lambda x: x)
     c3 = c2.map(lambda x: x)
 
@@ -17,7 +40,7 @@ def test_chaining() -> None:
 
 
 def test_uncalled_count() -> None:
-    c1 = rs.IteratorOfInt([1, 2, 3])
+    c1 = rs.AnyIterator([1, 2, 3])
 
     assert c1.uncalled == 0
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use std::collections::VecDeque;
 
+use itertools::Itertools;
 use pyo3::{
     prelude::*,
-    types::{PyFunction, PyInt, PyList},
+    types::{PyFunction, PyList},
 };
 
 enum SupportedIterableTypes {
@@ -11,29 +12,33 @@ enum SupportedIterableTypes {
 
 // TODO: generate with macro based on `SupportedIterableTypes`
 #[pyclass]
-struct IteratorOfInt {
-    it: Box<dyn Iterator<Item = Py<PyInt>> + Send + Sync>,
+struct AnyIterator {
+    it: Box<dyn Iterator<Item = Py<PyAny>> + Send + Sync>,
     to_apply: VecDeque<Py<PyFunction>>,
 }
 
 #[pymethods]
-impl IteratorOfInt {
+impl AnyIterator {
     #[new]
-    fn py_new(list: &Bound<'_, PyList>) -> PyResult<Self> {
-        let ints = list
-            .iter()
-            .map(|x| x.downcast_into::<PyInt>().map(pyo3::Bound::unbind))
-            .collect::<Result<Vec<_>, _>>()?;
-
-        Ok(IteratorOfInt {
-            it: Box::new(ints.into_iter()),
+    fn py_new(list: &Bound<'_, PyList>) -> Self {
+        AnyIterator {
+            it: Box::new(
+                list.iter()
+                    .map(pyo3::Bound::unbind)
+                    .collect_vec()
+                    .into_iter(),
+            ),
             to_apply: VecDeque::new(),
-        })
+        }
     }
 
     fn map<'a>(mut slf: PyRefMut<'a, Self>, f: Bound<'_, PyFunction>) -> PyRefMut<'a, Self> {
         slf.to_apply.push_back(f.unbind());
         slf
+    }
+
+    fn to_list<'a>(mut slf: PyRefMut<'a, Self>, py: Python<'a>) -> PyResult<Bound<'a, PyList>> {
+        PyList::new(py, slf.it.by_ref().collect_vec())
     }
 
     #[getter]
@@ -43,7 +48,8 @@ impl IteratorOfInt {
 }
 
 #[pymodule]
+#[allow(clippy::unnecessary_wraps)]
 fn _py_combinator(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    let _ = m.add_class::<IteratorOfInt>();
+    let _ = m.add_class::<AnyIterator>();
     Ok(())
 }
