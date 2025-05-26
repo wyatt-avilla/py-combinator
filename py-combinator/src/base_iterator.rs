5be71f2262bffcbd4aea9ced9a17a6dc7c70c6cf
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-05-26 00:51:33 -0700

    add macro attribute parameter to define `self` replacement candidates

--- py-combinator/src/base_iterator.rs
+++ py-combinator/src/base_iterator.rs
@@ -11,7 +11,7 @@ impl PyBaseIterator {
     }
 }
 
-#[macros::register_methods]
+#[macros::register_methods(self_generic = S)]
 impl crate::base_iterator::PyBaseIterator {
     pub fn new(
         iter: Box<dyn Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>> + Send + Sync>,
@@ -19,20 +19,20 @@ impl crate::base_iterator::PyBaseIterator {
         Self { iter }
     }
 
-    pub fn to_list<I>(iter: I) -> pyo3::PyResult<pyo3::Py<pyo3::types::PyList>>
+    pub fn to_list<S>(iter: S) -> pyo3::PyResult<pyo3::Py<pyo3::types::PyList>>
     where
-        I: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
+        S: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
     {
         let v = iter.collect::<pyo3::PyResult<Vec<_>>>()?;
         pyo3::Python::with_gil(|py| Ok(pyo3::types::PyList::new(py, v)?.unbind()))
     }
 
-    pub fn filter<I>(
-        iter: I,
+    pub fn filter<S>(
+        iter: S,
         f: pyo3::Py<pyo3::types::PyFunction>,
-    ) -> std::iter::Filter<I, impl FnMut(&pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>) -> bool>
+    ) -> std::iter::Filter<S, impl FnMut(&pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>) -> bool>
     where
-        I: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
+        S: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
     {
         let bad_predicate = "exception in filter predicate";
 
@@ -52,29 +52,29 @@ impl crate::base_iterator::PyBaseIterator {
     }
 
     #[allow(clippy::type_complexity)]
-    pub fn map<I>(
-        iter: I,
+    pub fn map<S>(
+        iter: S,
         f: pyo3::Py<pyo3::types::PyFunction>,
     ) -> std::iter::Map<
-        I,
+        S,
         impl FnMut(
             pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>,
         ) -> pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>,
     >
     where
-        I: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
+        S: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
     {
         iter.map(move |x| pyo3::Python::with_gil(|py| x.and_then(|x| f.call1(py, (x.bind(py),)))))
     }
 
     #[allow(clippy::needless_pass_by_value)] // for f
-    pub fn fold<I>(
-        mut iter: I,
+    pub fn fold<S>(
+        mut iter: S,
         init: pyo3::Py<pyo3::types::PyAny>,
         f: pyo3::Py<pyo3::types::PyFunction>,
     ) -> pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>
     where
-        I: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
+        S: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
     {
         pyo3::Python::with_gil(|py| {
             iter.try_fold(init, |a, x| x.and_then(|x| f.call1(py, (&a, x))))
@@ -82,25 +82,25 @@ impl crate::base_iterator::PyBaseIterator {
     }
 
     #[allow(clippy::type_complexity)]
-    pub fn enumerate<I>(
-        iter: I,
+    pub fn enumerate<S>(
+        iter: S,
     ) -> std::iter::Map<
-        std::iter::Enumerate<I>,
+        std::iter::Enumerate<S>,
         impl FnMut(
             (usize, pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>),
         ) -> pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>,
     >
     where
-        I: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
+        S: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
     {
         use pyo3::IntoPyObjectExt;
         iter.enumerate()
             .map(move |(i, x)| pyo3::Python::with_gil(|py| x.and_then(|x| (i, x).into_py_any(py))))
     }
 
-    pub fn take<I>(iter: I, n: usize) -> std::iter::Take<I>
+    pub fn take<S>(iter: S, n: usize) -> std::iter::Take<S>
     where
-        I: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
+        S: Iterator<Item = pyo3::PyResult<pyo3::Py<pyo3::types::PyAny>>>,
     {
         iter.take(n)
     }
