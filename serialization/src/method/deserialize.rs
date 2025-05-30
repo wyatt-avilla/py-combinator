347b18b9fa8ff39355ce73b51df09edf739ced18
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-05-30 15:42:19 -0700

    error enum for method deserialize

--- serialization/src/method/deserialize.rs
+++ serialization/src/method/deserialize.rs
@@ -3,15 +3,38 @@ use syn::{Ident, ImplItemFn, parse_str};
 
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
+use thiserror::Error;
 
 use crate::{ImplBlock, Method};
 
-impl Method {
-    pub fn into_impl_item(&self, impl_block: &ImplBlock) -> Result<ImplItemFn, String> {
-        let qualified_trait_name =
-            parse_str::<syn::Path>(impl_block.nice_name().as_ref()).map_err(|e| e.to_string())?;
+#[derive(Debug, Error)]
+pub enum MethodDeserializeError {
+    #[error("Couldn't parse Method name into a `syn::Path`")]
+    NamePathParseError(String),
 
-        let self_name = parse_str::<Ident>(&self.name).map_err(|e| e.to_string())?;
+    #[error("Couldn't parse name into a `syn::Ident`")]
+    NameParseError(String),
+
+    #[error("Method field was empty")]
+    EmptyField,
+
+    #[error("Couldn't parse type into a `syn::Type`")]
+    ArgTypeParseError(String),
+
+    #[error("Couldn't parse into `TokenStream`")]
+    TokenStreamParseError(String),
+}
+
+impl Method {
+    pub fn into_impl_item(
+        &self,
+        impl_block: &ImplBlock,
+    ) -> Result<ImplItemFn, MethodDeserializeError> {
+        let qualified_trait_name = parse_str::<syn::Path>(impl_block.nice_name().as_ref())
+            .map_err(|e| MethodDeserializeError::NameParseError(e.to_string()))?;
+
+        let self_name = parse_str::<Ident>(&self.name)
+            .map_err(|e| MethodDeserializeError::NamePathParseError(e.to_string()))?;
         let arg_names: Vec<Ident> = self
             .args
             .iter()
@@ -23,19 +46,20 @@ impl Method {
                 }
             })
             .collect::<Result<Vec<_>, _>>()
-            .map_err(|e| e.to_string())?;
+            .map_err(|e| MethodDeserializeError::NameParseError(e.to_string()))?;
 
         let typed_args: TokenStream2 = Itertools::intersperse(
             self.args
                 .iter()
                 .filter(|arg| arg.expected_type != impl_block.self_generic)
                 .map(|arg| {
-                    let name = parse_str::<Ident>(&arg.name).map_err(|e| e.to_string())?;
-                    let ty =
-                        parse_str::<syn::Type>(&arg.expected_type).map_err(|e| e.to_string())?;
+                    let name = parse_str::<Ident>(&arg.name)
+                        .map_err(|e| MethodDeserializeError::NameParseError(e.to_string()))?;
+                    let ty = parse_str::<syn::Type>(&arg.expected_type)
+                        .map_err(|e| MethodDeserializeError::ArgTypeParseError(e.to_string()))?;
                     Ok(quote! { #name: #ty })
                 })
-                .collect::<Result<Vec<_>, String>>()?
+                .collect::<Result<Vec<_>, _>>()?
                 .into_iter(),
             quote! { , },
         )
@@ -45,23 +69,30 @@ impl Method {
             Itertools::intersperse(arg_names.iter().map(|name| quote! { #name }), quote! { , })
                 .collect();
 
-        let self_function: TokenStream2 =
-            parse_str(&impl_block.self_function.clone()).map_err(|e| e.to_string())?;
+        let self_function: TokenStream2 = parse_str(&impl_block.self_function.clone())
+            .map_err(|e| MethodDeserializeError::TokenStreamParseError(e.to_string()))?;
 
         let return_tokens: TokenStream2 = {
             let return_type = if self.literal_return {
                 self.return_type
                     .as_ref()
-                    .map(|ret| parse_str::<syn::Type>(ret).map_err(|e| e.to_string()))
+                    .map(|ret| {
+                        parse_str::<syn::Type>(ret)
+                            .map_err(|e| MethodDeserializeError::ArgTypeParseError(e.to_string()))
+                    })
                     .transpose()?
             } else {
                 if self.strips.is_empty() {
-                    Some(parse_str::<syn::Type>("Self").map_err(|e| e.to_string())?)
+                    Some(
+                        parse_str::<syn::Type>("Self").map_err(|e| {
+                            MethodDeserializeError::ArgTypeParseError(e.to_string())
+                        })?,
+                    )
                 } else {
                     let impl_name = impl_block
                         .name
                         .last()
-                        .ok_or("Empty impl block name".to_string())?;
+                        .ok_or(MethodDeserializeError::EmptyField)?;
                     todo!()
                 }
             };
