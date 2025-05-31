2e7b25df34093b5e3136d1e3b05ba98ba4f84eda
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-05-30 23:04:05 -0700

    inject as many methods as possible w/o compile errors

--- py-combinator/src/double_ended_iterator.rs
+++ py-combinator/src/double_ended_iterator.rs
@@ -31,31 +31,11 @@ impl crate::double_ended_iterator::PyDoubleEndedIterator {
 #[macros::add_trait_methods(PyBaseIterator)]
 #[pymethods]
 impl PyDoubleEndedIterator {
-    //pub fn to_list(&mut self) -> PyResult<Py<PyList>> {
-    //    PyBaseIterator::to_list(&mut self.iter)
-    //}
-
-    //pub fn map(&mut self, f: Py<PyFunction>) -> Self {
-    //    Self::new(Box::new(PyBaseIterator::map(self.take_inner(), f)))
-    //}
-
-    //pub fn filter(&mut self, f: Py<PyFunction>) -> Self {
-    //    PyDoubleEndedIterator::new(Box::new(PyBaseIterator::filter(self.take_inner(), f)))
-    //}
-
-    //#[allow(clippy::needless_pass_by_value)] // for f
-    //pub fn fold(&mut self, init: Py<PyAny>, f: Py<PyFunction>) -> PyResult<Py<PyAny>> {
-    //    Python::with_gil(|py| {
-    //        self.take_inner()
-    //            .try_fold(init, |a, x| x.and_then(|x| f.call1(py, (&a, x))))
-    //    })
-    //}
-
-    //pub fn take(&mut self, n: usize) -> Self {
-    //    Self::new(Box::new(
-    //        PyBaseIterator::take(self.iter.by_ref(), n)
-    //            .collect_vec()
-    //            .into_iter(),
-    //    ))
-    //}
+    pub fn valid_take_implementation(&mut self, n: usize) -> Self {
+        Self::new(Box::new(
+            crate::iterators::PyBaseIterator::take(self.iter.by_ref(), n)
+                .collect::<Vec<_>>()
+                .into_iter(),
+        ))
+    }
 }
--- py-combinator/src/sized_double_ended_iterator.rs
+++ py-combinator/src/sized_double_ended_iterator.rs
@@ -1,10 +1,4 @@
-use itertools::Itertools;
-use pyo3::{
-    prelude::*,
-    types::{PyFunction, PyList},
-};
-
-use crate::{base_iterator::PyBaseIterator, double_ended_iterator::PyDoubleEndedIterator};
+use pyo3::prelude::*;
 
 pub trait SizedDoubleEndedIterator: Iterator + DoubleEndedIterator + ExactSizeIterator {}
 impl<T> SizedDoubleEndedIterator for T where T: Iterator + DoubleEndedIterator + ExactSizeIterator {}
@@ -30,41 +24,6 @@ impl crate::sized_double_ended_iterator::PySizedDoubleEndedIterator {
     }
 }
 
+#[macros::add_trait_methods(PyBaseIterator, PyDoubleEndedIterator)]
 #[pymethods]
-impl PySizedDoubleEndedIterator {
-    pub fn to_list(&mut self) -> PyResult<Py<PyList>> {
-        PyBaseIterator::to_list(&mut self.iter)
-    }
-
-    pub fn map(&mut self, f: Py<PyFunction>) -> Self {
-        Self::new(Box::new(PyBaseIterator::map(self.take_inner(), f)))
-    }
-
-    pub fn filter(&mut self, f: Py<PyFunction>) -> PyDoubleEndedIterator {
-        PyDoubleEndedIterator::new(Box::new(PyBaseIterator::filter(self.take_inner(), f)))
-    }
-
-    #[allow(clippy::needless_pass_by_value)] // for f
-    pub fn fold(&mut self, init: Py<PyAny>, f: Py<PyFunction>) -> PyResult<Py<PyAny>> {
-        Python::with_gil(|py| {
-            self.take_inner()
-                .try_fold(init, |a, x| x.and_then(|x| f.call1(py, (&a, x))))
-        })
-    }
-
-    pub fn enumerate(&mut self) -> Self {
-        Self::new(Box::new(PyBaseIterator::enumerate(self.take_inner())))
-    }
-
-    pub fn take(&mut self, n: usize) -> Self {
-        Self::new(Box::new(
-            PyBaseIterator::take(self.iter.by_ref(), n)
-                .collect_vec()
-                .into_iter(),
-        ))
-    }
-
-    pub fn rev(&mut self) -> Self {
-        Self::new(Box::new(PyDoubleEndedIterator::rev(self.take_inner())))
-    }
-}
+impl PySizedDoubleEndedIterator {}
