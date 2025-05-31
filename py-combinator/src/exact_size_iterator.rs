767978e479192aea7a4c5dad7920c1de192bc94a
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-05-30 22:54:06 -0700

    consolidate exact size iter implementation

--- py-combinator/src/exact_size_iterator.rs
+++ py-combinator/src/exact_size_iterator.rs
@@ -1,10 +1,4 @@
-use itertools::Itertools;
-use pyo3::{
-    prelude::*,
-    types::{PyFunction, PyList},
-};
-
-use crate::base_iterator::PyBaseIterator;
+use pyo3::prelude::*;
 
 type PyExactSizeIteratorT = Box<dyn ExactSizeIterator<Item = PyResult<Py<PyAny>>> + Send + Sync>;
 #[pyclass]
@@ -26,37 +20,6 @@ impl crate::exact_size_iterator::PyExactSizeIterator {
     }
 }
 
+#[macros::add_trait_methods(PyBaseIterator)]
 #[pymethods]
-impl PyExactSizeIterator {
-    pub fn to_list(&mut self) -> PyResult<Py<PyList>> {
-        PyBaseIterator::to_list(&mut self.iter)
-    }
-
-    pub fn map(&mut self, f: Py<PyFunction>) -> Self {
-        Self::new(Box::new(PyBaseIterator::map(self.take_inner(), f)))
-    }
-
-    pub fn filter(&mut self, f: Py<PyFunction>) -> PyBaseIterator {
-        PyBaseIterator::new(Box::new(PyBaseIterator::filter(self.take_inner(), f)))
-    }
-
-    #[allow(clippy::needless_pass_by_value)] // for f
-    pub fn fold(&mut self, init: Py<PyAny>, f: Py<PyFunction>) -> PyResult<Py<PyAny>> {
-        Python::with_gil(|py| {
-            self.take_inner()
-                .try_fold(init, |a, x| x.and_then(|x| f.call1(py, (&a, x))))
-        })
-    }
-
-    pub fn enumerate(&mut self) -> Self {
-        Self::new(Box::new(PyBaseIterator::enumerate(self.take_inner())))
-    }
-
-    pub fn take(&mut self, n: usize) -> Self {
-        Self::new(Box::new(
-            PyBaseIterator::take(self.iter.by_ref(), n)
-                .collect_vec()
-                .into_iter(),
-        ))
-    }
-}
+impl PyExactSizeIterator {}
